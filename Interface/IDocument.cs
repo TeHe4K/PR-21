b1ae29fc1d33670cbfb532f7bc98505a6051ff85
TeHe4K <hryblackwii@gmail.com>
2026-02-16 07:24:42 +0500

    Реализованные интерфейсы. Коневский

---
+++ Interface/IDocument.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konevskii_PR21.Interface
+{
+    public interface IDocument
+    {
+        void Save(bool Update = false);
+        List<Model.Document> AllDocuments();
+        void Delete();
+    }
+}
