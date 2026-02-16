6a2ff39b6f3b77ebe809220ec54f3ba48c2f1653
TeHe4K <hryblackwii@gmail.com>
2026-02-16 07:30:42 +0500

    DBConnection

---
+++ Classes/Common/DBConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konevskii_PR21.Classes.Common
+{
+    public class DBConnection
+    {
+        public static readonly string Parh = @"";
+
+        public static OleDbConnection Connection()
+        {
+            OleDbConnection oleDbConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + Parh);
+
+            oleDbConnection.Open();
+            return oleDbConnection;
+        }
+
+        public static OleDbDataReader Query(string Query, OleDbConnection Connection)
+        {
+            return new OleDbCommand(Query, Connection).ExecuteReader();
+        }
+
+        public static void CloseConnection(OleDbConnection oleDbConnection)
+        {
+            oleDbConnection.Close();
+        }
+    }
+}
